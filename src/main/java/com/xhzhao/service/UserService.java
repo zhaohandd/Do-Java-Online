15db2073d628e73e17c2565aa2e07f0a64d51824
赵晗 <37007328+zhaohandd@users.noreply.github.com>
2019-05-03 22:23:17 +0800

    Add files via upload

---
+++ src/main/java/com/xhzhao/service/UserService.java
@@ -0,0 +1,65 @@
+package com.xhzhao.service;
+
+import com.xhzhao.domain.Result;
+import com.xhzhao.domain.User;
+import com.xhzhao.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(rollbackFor = RuntimeException.class)
+public class UserService {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    public Result register(User user) {
+        Result result = new Result();
+        result.setSuccess(false);
+        result.setDetail(null);
+        try {
+            User existUser = userMapper.findUserByName(user.getUsername());
+            if(existUser != null){
+                //如果用户名已存在
+                result.setMsg("用户名已存在");
+            }else{
+                userMapper.register(user);
+                //System.out.println(user.getId());
+                result.setMsg("注册成功");
+                result.setSuccess(true);
+                result.setDetail(user);
+            }
+        } catch (Exception e) {
+            result.setMsg(e.getMessage());
+            e.printStackTrace();
+        }
+        return result;
+
+    }
+
+    public Result login(User user) {
+        Result result = new Result();
+        result.setSuccess(false);
+        result.setDetail(null);
+        try {
+            Integer userId= userMapper.login(user);
+            if(userId == null){
+                result.setMsg("用户名或密码错误");
+            }else{
+                result.setMsg("登录成功");
+                result.setSuccess(true);
+                user.setId(userId);
+                result.setDetail(user);
+            }
+
+        } catch (Exception e) {
+            result.setMsg(e.getMessage());
+            e.printStackTrace();
+        }
+        return result;
+
+    }
+
+}
+
